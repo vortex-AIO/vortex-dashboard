3fd7e7ad615880de14177656636ec50f45f95a7c
Playfair <74739795+playfairs@users.noreply.github.com>
2025-06-21 09:55:30 -0500

    added docs page

---
+++ src/app/(routes)/docs/page.tsx
@@ -0,0 +1,9 @@
+// /app/routes/docs/page.tsx
+export default function DocsPage() {
+    return (
+        <main className="min-h-screen p-10 text-center">
+            <h1 className="text-4xl font-bold">Documentation</h1>
+            <p className="mt-4 text-lg">No Documementation yet.</p>
+        </main>
+    );
+}
