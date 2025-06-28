1b234765aec8376cbc0128990bd2b9700638d8f1
Playfair <74739795+playfairs@users.noreply.github.com>
2025-06-27 22:36:22 -0500

    .

--- src/app/(routes)/418/route.ts
+++ src/app/(routes)/418/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
 
 export function GET() {
-  return new NextResponse("I'm a teapot", { status: 418 });
+  return new NextResponse("I'm a teapot.", { status: 418 });
 }
